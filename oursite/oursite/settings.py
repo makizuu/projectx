56e981fd074f43b87d9ea0e4adc1c237643a7d34
Ubuntu <REEEE@ubuntuProject.zzqog2utrquunh1uthxoz1ogoc.fx.internal.cloudapp.net>
2022-02-22 10:53:44 +0000

    Login redirectaa libraryyn, mutta create ei toimi

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,5 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/library/'
 LOGOUT_REDIRECT_URL = 'home'
